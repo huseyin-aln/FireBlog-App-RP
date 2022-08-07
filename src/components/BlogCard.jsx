4d787765e2bf88a9c610170eebad0d8b9031d3d1
huseyin <huseyinarslan06@gmail.com>
2022-08-07 12:03:39 +0300

    data feched

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
+
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
@@ -8,14 +8,28 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import { useParams, useNavigate } from "react-router-dom";
 
 export default function BlogCard({ blogCard }) {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { author, content, date, image, title } = blogCard;
+
+  // const date1 =
+  //   blogCard.date[2] +
+  //   " " +
+  //   blogCard.date[1] +
+  //   " " +
+  //   blogCard.date[3] +
+  //   " , " +
+  //   blogCard.date[0];
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345 }} onClick={() => navigate("/details/" + id)}>
       <CardMedia
         component="img"
         height="194"
-        image={blogCard.imageUrl}
+        image={blogCard.image}
         alt="image"
       />
       <CardContent>
@@ -27,9 +41,19 @@ export default function BlogCard({ blogCard }) {
         >
           {blogCard.title}
         </Typography>
+        <Typography
+          variant="h6"
+          color="primary"
+          sx={{ fontFamily: "Girassol" }}
+        >
+          {blogCard.date}
+        </Typography>
         <Typography variant="body2" color="text.secondary">
           {blogCard.content}
         </Typography>
+      </CardContent>
+      <CardContent>
+        <Typography variant="h6">{blogCard.author}</Typography>
       </CardContent>
 
       <CardActions disableSpacing>
